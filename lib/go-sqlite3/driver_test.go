e02eafb42dc2cdcc111c22f4f15ff9357b8afbe2
jakoblorz <jakob@lorz.org>
2019-12-12 22:42:57 +0100

    Implement sql_dialect for sqlite3, start testing of sqlite3 driver

---
+++ lib/go-sqlite3/driver_test.go
@@ -0,0 +1,124 @@
+package sqlite3
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/jakoblorz/dynsql"
+
+	sqlite3 "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	testJSON_s string
+	testJSON_b []byte
+	testJSON_m = map[string]interface{}{
+		"name": "test_user",
+		"age":  6,
+	}
+)
+
+func init() {
+	var err error
+	testJSON_b, err = json.Marshal(testJSON_m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	testJSON_s = string(testJSON_b)
+}
+
+func wrapRawConn(c driver.Conn) dynsql.ExecerQueryerPreparer {
+	return &rawSQLConnWrapper{c}
+}
+
+type rawSQLConnWrapper struct {
+	c driver.Conn
+}
+
+func (r *rawSQLConnWrapper) Prepare(query string) (driver.Stmt, error) {
+	return r.c.Prepare(query)
+}
+
+func (r *rawSQLConnWrapper) Exec(query string, args []driver.Value) (driver.Result, error) {
+	stmt, err := r.c.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	return stmt.Exec(args)
+}
+
+func (r *rawSQLConnWrapper) Query(query string, args []driver.Value) (driver.Rows, error) {
+	stmt, err := r.c.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	return stmt.Query(args)
+}
+
+func wrapOnConnExtractor(baseDriver driver.Driver, sql dynsql.SQLDialect) *connExtractor {
+	return &connExtractor{
+		Driver: *dynsql.WrapDriver(baseDriver, sql),
+	}
+}
+
+type connExtractor struct {
+	dynsql.Driver
+	conn driver.Conn
+}
+
+func (c *connExtractor) getLatestConn() driver.Conn {
+	return c.conn
+}
+
+func (c *connExtractor) Open(name string) (driver.Conn, error) {
+	var err error
+	c.conn, err = c.Driver.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return c.conn, nil
+}
+
+func Test(t *testing.T) {
+	d := wrapOnConnExtractor(&sqlite3.SQLiteDriver{}, SQLite3Dialect(0))
+	sql.Register("dyn-test-sqlite3", d)
+
+	db, err := sql.Open("dyn-test-sqlite3", "")
+	if !assert.NoError(t, err, "expected no error opening transient in-memory database") {
+		t.FailNow()
+	}
+	defer db.Close()
+
+	tableName := "test"
+	log.Printf("%s\n", testJSON_s)
+	_, err = db.Exec(fmt.Sprintf("INSERT INTO %s JSON %s;", tableName, testJSON_s))
+	if !assert.NoError(t, err, "expected no error inserting custom JSON payload: %+v") {
+		t.FailNow()
+	}
+
+	dialect := SQLite3Dialect(0)
+	tables, err := dialect.GetAllTableNames(wrapRawConn(d.getLatestConn()))
+	if !assert.NoError(t, err, "expected no error when listing all tables") {
+		t.FailNow()
+	}
+	if !assert.Contains(t, tables, tableName, "expected table list to contain table %s", tableName) {
+		t.FailNow()
+	}
+	columns, err := dialect.GetAllTableColumns(tableName, wrapRawConn(d.getLatestConn()))
+	if !assert.NoError(t, err, "expected no error when listing all columns of table %s", tableName) {
+		t.FailNow()
+	}
+	for k := range testJSON_m {
+		if !assert.Contains(t, columns, k) {
+			t.FailNow()
+		}
+		if !assert.Equal(t, columns[k], DefaultFallbackType) {
+			t.FailNow()
+		}
+	}
+}
--- lib/go-sqlite3/sql_dialect.go
+++ lib/go-sqlite3/sql_dialect.go
@@ -2,6 +2,7 @@ package sqlite3
 
 import (
 	"database/sql/driver"
+	"fmt"
 	"regexp"
 	"strings"
 
@@ -15,7 +16,27 @@ const SelectAllTableNamesSQL = `SELECT name FROM sqlite_master
 const SelectAllTableColumnsSQL = `SELECT sql FROM sqlite_master
 	WHERE type = "table" AND name = $1;`
 
+const CreateNewTableSQL = `CREATE TABLE %s (
+	%s
+);`
+
+const AlterTableAddColumnSQL = `ALTER TABLE %s
+	ADD COLUMN $1 $2;`
+
+const InsertValuesSQL = `INSERT INTO %s (
+	%s
+) VALUES (
+	%s
+);`
+
+const DefaultFallbackType = "TEXT"
+
 var (
+	DefaultTypeMap = map[string]string{
+		dynsql.InheritedIDField:         fmt.Sprintf("%s PRIMARY KEY", DefaultFallbackType),
+		dynsql.InheritedUpdatedAtField:  DefaultFallbackType,
+		dynsql.InheritedInsertedAtField: DefaultFallbackType,
+	}
 	ColumnNameAndTypeRegex = regexp.MustCompile(`(?:\(|\,\s*)(?P<Key>[a-zA-Z\_\-]+)(?:\s*)(?P<Type>[a-zA-Z\_\-]+)(?:\s*)`)
 )
 
@@ -86,15 +107,40 @@ func (s SQLite3Dialect) GetAllTableColumns(tableName string, x dynsql.ExecerQuer
 	return v, nil
 }
 
-func (s SQLite3Dialect) CreateNewTable(table string, keys []string, x dynsql.ExecerQueryer) error {
-	return nil
+func (s SQLite3Dialect) CreateNewTable(tableName string, keys []string, x dynsql.ExecerQueryer) error {
+	query := ""
+	for _, k := range keys {
+		t, ok := DefaultTypeMap[k]
+		if !ok {
+			t = DefaultFallbackType
+		}
+		query = fmt.Sprintf("%s, %s %s", query, k, t)
+	}
+	query = query[2:]
 
+	_, err := x.Exec(fmt.Sprintf(CreateNewTableSQL, tableName, query), nil)
+	return err
 }
 
-func (s SQLite3Dialect) AddColumnToTable(table, key string, x dynsql.ExecerQueryer) error {
-	return nil
+func (s SQLite3Dialect) AddColumnToTable(tableName, key string, x dynsql.ExecerQueryer) error {
+	t, ok := DefaultTypeMap[key]
+	if !ok {
+		t = DefaultFallbackType
+	}
+
+	_, err := x.Exec(fmt.Sprintf(AlterTableAddColumnSQL, tableName), []driver.Value{key, t})
+	return err
 }
 
-func (s SQLite3Dialect) InsertValuesPrepare(table string, keys []string, x dynsql.ExecerQueryerPreparer) (driver.Stmt, error) {
-	return nil, nil
+func (s SQLite3Dialect) InsertValuesPrepare(tableName string, keys []string, x dynsql.ExecerQueryerPreparer) (driver.Stmt, error) {
+	columnNames := ""
+	placeholders := ""
+	for i, k := range keys {
+		columnNames = fmt.Sprintf("%s, %s", columnNames, k)
+		placeholders = fmt.Sprintf("%s, $%d", placeholders, i)
+	}
+	columnNames = columnNames[2:]
+	placeholders = placeholders[2:]
+
+	return x.Prepare(fmt.Sprintf(InsertValuesSQL, tableName, columnNames, placeholders))
 }
